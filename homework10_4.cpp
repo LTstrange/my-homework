38db2fb1a4522f6a8a8b655cef0ba50272cd6449
LTstrange <33590343+LTstrange@users.noreply.github.com>
2018-11-07 13:36:42 +0800

    Add files via upload

---
+++ homework10_4.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void Input(int a,int *n){
+	for (int i=0;i<a;i++){
+		cin>>n[i];
+	}
+}
+
+int main(){
+	int n1[100]={0};
+	int n2[100]={0};
+	int a1=0,a2=0;
+	cin>>a1;
+	Input(a1,n1);
+	cin>>a2;
+	Input(a2,n2);
+	for (int i=0;i<a1;i++){
+		
+	}
+	return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+#include<time.h>
+#include<stdlib.h>
+using namespace std;
+
+void initialize(int field[]){
+	for (int i=0;i<20;i++){
+		field[0][i]=
+	}
+}
+
+int main(){
+	srand((unsigned)time(NULL));
+	int field[20][20]={0};
+	initialize(field);
+	
+	
+
+
+	return 0;
+}
